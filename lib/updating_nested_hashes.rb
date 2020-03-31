9455a3d4072d7ec3286b63ee9153d456a39940e7
sofia tofigh <sst2140@columbia.edu>
2020-03-31 04:53:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -49,9 +49,7 @@ def add_characters
          :heroine => {name: "Juliet", age: "15", status: "alive"},
          :heroine_friends => [
          {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
-         ]
-      }
+         {name: "Nurse", age: "44", attitude: "worried"}]}}
      
      epic_tragedy[:additional_characters] = []
      
