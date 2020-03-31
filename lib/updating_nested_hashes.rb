27e24c0af046b63257ed19bd537bce5a1036ea72
sofia tofigh <sst2140@columbia.edu>
2020-03-31 04:50:07 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -52,9 +52,10 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :additional_characters => []
+      :additional_characters => []}
       
-   }
+      epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
+      epic_tragedy[:additional_characters][1] = { name: "Apothecary" }
 
    epic_tragedy
 end
